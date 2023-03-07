6ef826507768f35d2de85ce1a6528784aae1f54e
verena <9377970+vpchung@users.noreply.github.com>
2023-03-07 11:02:33 -0800

    fix format; update comments

--- update_valid_values.py
+++ update_valid_values.py
@@ -1,7 +1,7 @@
 """Update Valid Values
 
-For every module listed in the mapping file, update the list of
-valid values in annotationProperty.csv.
+For every module listed in the mapping file, update the list of valid
+values in annotationProperty.csv.
 """
 
 import os
@@ -9,25 +9,30 @@ import os
 import yaml
 import pandas as pd
 
+PARENT_DIR = "modules"
 
 def main(mapping):
     """Main function."""
     for module, attributes in mapping.items():
-        to_update = os.path.join("modules", module, "annotationProperty.csv")
+        to_update = os.path.join(PARENT_DIR, module, "annotationProperty.csv")
+
+        # Read all columns as strings, so that `TRUE/FALSE` values are
+        # not rewritten as `True/False`. NaN values should also not be
+        # used.
         df = (pd.read_csv(to_update, dtype=str, keep_default_na=False)
               .set_index('Attribute'))
-        # Replace list of controlled-vocabulary terms with latest of a given attribute.
+
+        # Replace list of valid values with latest terms for the
+        # given attribute.
         for attribute in attributes:
-            cv_terms = (pd.read_csv(os.path.join("modules", attribute['src']), dtype=str)
-                        .Attribute
+            cv_file = os.path.join(PARENT_DIR, attribute['src'])
+            cv_terms = (pd.read_csv(cv_file, dtype=str)['Attribute']
                         .tolist())
             df.loc[attribute['name'], 'Valid Values'] = ", ".join(cv_terms)
-        df.reset_index().to_csv(to_update, index=False)
+        df.to_csv(to_update)
 
 
 if __name__ == "__main__":
-    # Get mappings as a dict, where
-    #   key = module name
-    #   value = list of attributes with valid value to update
-    mappings = yaml.safe_load(open("modules/mapping.yaml"))
-    main(mappings)
+    with open(os.path.join(PARENT_DIR, "mapping.yaml")) as f:
+        mapping_cfg = yaml.safe_load(f)
+    main(mapping_cfg)
