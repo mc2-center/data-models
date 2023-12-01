bf0ec99ab4fde6cb6ad63be6b950f6753717ba9c
Orion Banks <49208907+Bankso@users.noreply.github.com>
2023-12-01 15:14:38 -0800

    Create release_workflow.sh
    Adds a simple bash script to update valid values, build CSV/JSON-LD, and create XLSX templates when prepping model releases

---
+++ release_workflow.sh
@@ -0,0 +1,31 @@
+#/usr/bin/bash
+
+#Usage: bash release_workflow.sh <path/to/config.yml> <path/to/model/repo>
+
+#This script will:
+#- update controlled vocabulary sets listed in annotationProperty.csv files and the all_valid_values.csv
+#- build a model CSV and convert to JSON-LD, using schematic
+#- create XLSX template files, saving them in a new 'template' folder
+
+#Intended for use when creating a data model release package.
+
+#Requires: .synapseConfig available in environment, python, schematic 
+
+#Model repo path should contain 'update_valid_values.py' and a 'Makefile' defining how the CSV and JSON-LD are built
+
+config="$1"
+homedir="$2"
+datatypes="Biospecimen PersonView PublicationView GrantView ToolView EducationalResource NanoStringGeoMxDSPLevel1 NanoStringGeoMxDSPLevel2 NanoStringGeoMxDSPLevel3 NanoStringGeoMxDSPImagingLevel2 NanoStringGeoMXROISegmentAnnotation"
+
+cd "$homedir"
+
+mkdir -p $homedir/templates
+
+python update_valid_values.py
+
+make
+
+for i in $datatypes;
+do
+	schematic manifest -c "$config" get -dt "$i" -oxlsx "$homedir/templates/$i.xlsx" || continue
+done
