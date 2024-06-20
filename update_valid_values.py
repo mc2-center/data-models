85d5437402560c567b923e50eb4c2185b05487d5
Verena Chung <9377970+vpchung@users.noreply.github.com>
2024-06-20 11:14:26 -0700

    [chore]: Update `update_valid_values.py` script (#105)
    * add non-preferred terms to file of all valid values
    
    * lint
    
    * update `all_valid_values.csv` file

--- update_valid_values.py
+++ update_valid_values.py
@@ -1,44 +1,56 @@
 """Update Valid Values
 
 For every module listed in the mapping file, update the list of valid
-values in annotationProperty.csv.  This will also generate a file 
+values in annotationProperty.csv.  This will also generate a file
 containing the full list of valid values for the data models.
 """
 
 import os
 
-import yaml
 import pandas as pd
+import yaml
 
 PARENT_DIR = "modules"
 
 
 def main(mapping):
     """Main function."""
-    all_terms = pd.DataFrame(columns=["category", "valid_value"])
+    all_terms = pd.DataFrame(
+        columns=["category", "valid_value", "nonpreferred_values"]
+    )
     for module, attributes in mapping.items():
         to_update = os.path.join(PARENT_DIR, module, "annotationProperty.csv")
 
         # Read all columns as strings, so that `TRUE/FALSE` values are
         # not rewritten as `True/False`. NaN values should also not be
         # used.
-        df = (pd.read_csv(to_update, dtype=str, keep_default_na=False)
-              .set_index('Attribute'))
+        df = (
+            pd.read_csv(to_update, dtype=str, keep_default_na=False)
+            .set_index("Attribute")
+        )
 
         for attribute in attributes:
-            cv_file = os.path.join(PARENT_DIR, attribute['src'])
-            cv_terms = pd.read_csv(cv_file, dtype=str, keep_default_na=False)['Attribute'].tolist()
-            # Replace list of valid values with latest terms for the
-            # given attribute.
-            df.loc[attribute['name'], 'Valid Values'] = ", ".join(cv_terms)
+            cv_file = os.path.join(PARENT_DIR, attribute["src"])
+            cv_df = pd.read_csv(
+                cv_file, dtype=str,
+                keep_default_na=False,
+                usecols=["Attribute", "Nonpreferred Terms"]
+            )
+            cv_terms = cv_df["Attribute"].tolist()
+            cv_nonpreferred = cv_df["Nonpreferred Terms"].tolist()
+
+            # Replace list of valid values in annotationProperty file
+            # with latest terms for the given attribute.
+            df.loc[attribute["name"], "Valid Values"] = ", ".join(cv_terms)
 
             # Add latest terms to list of all valid values.
             terms_to_add = pd.DataFrame()
-            terms_to_add['valid_value'] = cv_terms
-            terms_to_add['category'] = os.path.basename(cv_file).split(".")[0]
+            terms_to_add["valid_value"] = cv_terms
+            terms_to_add["nonpreferred_values"] = cv_nonpreferred
+            terms_to_add["category"] = os.path.basename(cv_file).split(".")[0]
             all_terms = pd.concat([all_terms, terms_to_add])
         df.to_csv(to_update)
-    all_terms.drop_duplicates().to_csv('all_valid_values.csv', index=False)
+    all_terms.drop_duplicates().to_csv("all_valid_values.csv", index=False)
 
 
 if __name__ == "__main__":
