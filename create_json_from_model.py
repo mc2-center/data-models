1cd13bc489eab793380da46a07664bb169d6b741
Orion Banks <49208907+Bankso@users.noreply.github.com>
2026-04-02 15:29:50 -0700

    Add script to build JSON with curator

---
+++ create_json_from_model.py
@@ -0,0 +1,16 @@
+from synapseclient import Synapse
+from synapseclient.extensions.curator import generate_jsonschema
+
+DATA_MODEL_SOURCE = "mc2.model.jsonld"
+DATA_TYPE = ["DataDSP", "Study", "FileView", "PublicationView", "GrantView", "ToolView", "EducationalResource", "DatasetView", "DataCatalog", "Biospecimen", "Individual", "Model"]
+OUTPUT_DIRECTORY = "./json_schemas"
+
+syn = Synapse()
+syn.login()
+
+schemas, file_paths = generate_jsonschema(
+    data_model_source=DATA_MODEL_SOURCE,
+    output=OUTPUT_DIRECTORY,
+    data_types=DATA_TYPE,
+    synapse_client=syn,
+)
